3122e84838d13a7db36ff977059f583e2c976f78
couldBeFree <zoriaalexandr@gmail.com>
2023-01-28 20:26:49 +0200

    implemented chart for home page

--- src/components/home/Home.vue
+++ src/components/home/Home.vue
@@ -40,44 +40,79 @@
           </v-card>
         </v-col>
       </v-row>
-      <DoughnutChart
-        :chart-data="data"
-        :options="options"
-        css-classes="chart-container"
-      />
+      <v-row>
+        <v-col sm="4">
+          <v-card color="primary">
+            <template v-slot:title>
+              <p class="content">
+                Expense By Category
+              </p>
+            </template>
+            <template v-slot:text>
+              <DoughnutChart
+                :chart-data="data"
+                :options="options"
+              />
+            </template>
+          </v-card>
+        </v-col>
+      </v-row>
     </v-responsive>
   </v-container>
 </template>
 
 <script>
 import { ref, computed } from "vue"
-import { DoughnutChart } from "vue-chart-3"
-import { Chart, DoughnutController, ArcElement } from 'chart.js'
-Chart.register(DoughnutController, ArcElement)
-const dataValues = ref([10, 20, 40])
+import { DoughnutChart } from 'vue-chart-3';
+import { Chart, registerables } from "chart.js";
+Chart.register(...registerables);
 
 export default {
   name: "home-item",
   setup() {
+    const dataValues = ref([300, 50, 100, 40, 90, 230]);
     const data = computed(() => ({
-      labels: ["Foo", "Bar", "Baz"],
+      labels: [
+        'Travel',
+        'Health Care',
+        'Garments',
+        'Grocery',
+        'Eating Outside',
+        'Entertainments',
+      ],
       datasets: [
         {
           data: dataValues.value,
-          backgroundColor: ["#859900", "#d33682", "#cb4b16"]
+          backgroundColor: [
+            'rgb(255, 99, 132)',
+            'rgb(54, 162, 235)',
+            'rgb(255, 205, 86)',
+            'rgba(12, 18, 30)',
+            'rgba(18, 235, 190)',
+            'rgba(235, 208, 18)',
+          ],
+          hoverOffset: 4
         }
       ]
     }))
 
     const options = ref({
+      responsive: true,
       plugins: {
+        legend: {
+          position: 'top',
+          labels: {
+            color: "white"
+          }
+        },
         title: {
+          display: false,
           text: "Doughnut"
         }
       }
     })
 
-    return { options, data }
+    return { data, options }
   },
   components: {
     DoughnutChart
