c756feb6b56d9bf21f28a47d1e6c32dec5c84d7a
couldBeFree <zoriaalexandr@gmail.com>
2023-01-29 19:38:44 +0200

    create category from ui

--- src/components/categories/Categories.vue
+++ src/components/categories/Categories.vue
@@ -3,11 +3,11 @@
     <v-responsive class="d-flex align-center text-center fill-height mt-9">
       <h1 class="mb-4">Categories</h1>
       <div>
-        {{remove}}
+        {{category}}
       </div>
       <v-row>
         <v-col sm="6" offset="3">
-          <v-card :loading="isLoading">
+          <v-card v-if="categories.length" :loading="isLoading">
             <template v-slot:text>
               <v-table>
                 <thead>
@@ -50,6 +50,17 @@
               </v-table>
             </template>
           </v-card>
+          <v-card class="pb-2" v-else>
+            <h2>There are no categories yet</h2>
+            <v-btn
+              class="mb-2 mt-2"
+              color="primary"
+              size="default"
+              @click="onCategoryOpen"
+            >
+              Create
+            </v-btn>
+          </v-card>
         </v-col>
       </v-row>
     </v-responsive>
@@ -80,6 +91,57 @@
         </v-card-actions>
       </v-card>
     </v-dialog>
+    <v-dialog
+      v-model="category.categoryDialog"
+      max-width="400"
+    >
+      <v-card>
+        <v-card-title class="text-h5 text-break">
+          {{category.categoryID.length ? 'Edit' : 'Add'}} category
+        </v-card-title>
+        <v-card-item>
+          <v-form
+            ref="form"
+            v-model="valid"
+            lazy-validation
+          >
+            <v-text-field
+              required
+              label="Category"
+              placeholder="Enter Category Name"
+              :rules="nameRule"
+              v-model="category.categoryName"
+              variant="underlined"
+            ></v-text-field>
+            <v-select
+              required
+              label="Select"
+              :rules="typeRule"
+              :items="['Expense', 'Income']"
+              v-model="category.categoryType"
+              variant="underlined"
+            ></v-select>
+          </v-form>
+        </v-card-item>
+        <v-card-actions>
+          <v-spacer></v-spacer>
+          <v-btn
+            color="primary"
+            variant="text"
+            @click="onCategoryAdd"
+          >
+            Add
+          </v-btn>
+          <v-btn
+            color="green-darken-1"
+            variant="text"
+            @click="category.categoryDialog = false"
+          >
+            Cancel
+          </v-btn>
+        </v-card-actions>
+      </v-card>
+    </v-dialog>
     <v-snackbar
       :timeout="3000"
       v-model="snackbar.isOpen"
@@ -103,18 +165,44 @@ export default {
     const removeDialog = ref(false);
     const categories = ref([]);
     const categoryToRemove = ref(null);
+    const form = ref();
+    const valid = ref(false);
 
     const snackbar = reactive({
       isOpen: false,
       text: '',
       type: 'success'
-    })
+    });
+
+    const category = reactive({
+      categoryDialog: false,
+      categoryName: '',
+      categoryType: '',
+      categoryID: ''
+    });
 
     const remove = reactive({
       removeDialog: false,
       removeName: '',
       itemToRemoveID: null,
     });
+
+    const onCategoryOpen = () => {
+      category.categoryName = '';
+      category.categoryID = '';
+      // category.categoryType = '';
+      category.categoryDialog = true;
+    };
+
+    const nameRule = ref([
+      v => !!v || 'Name is required',
+      v => v.length > 4 || 'Name should be at least 4 characters long',
+    ]);
+
+    const typeRule = ref([
+      v => !!v || 'Type is required',
+      v => v.length > 4 || 'Type should be at least 4 characters long',
+    ]);
 
     const onRemove = async () => {
       isLoading.value = true;
@@ -150,11 +238,35 @@ export default {
       }
     };
 
+    const onCategoryAdd = async () => {
+      isLoading.value = false;
+      const { valid } = await form.value.validate();
+      if (valid) {
+        isLoading.value = true;
+        try {
+          await axios.post('/create-category', {
+            name: category.categoryName,
+            type: category.categoryType
+          });
+          await getCategories();
+          snackbar.type = 'success';
+          snackbar.text = 'Category was successfully added';
+        } catch({ message }) {
+          snackbar.type = 'error';
+          snackbar.text = `${message}`;
+        } finally {
+          isLoading.value = false;
+          snackbar.isOpen = true;
+          category.categoryDialog = false;
+        }
+      }
+    };
+
     onMounted(() => {
       getCategories();
     });
 
-    return { categories, isLoading, removeDialog, categoryToRemove, onRemoveClick, remove, onRemove, snackbar };
+    return { categories, isLoading, form, valid, nameRule, typeRule, removeDialog, categoryToRemove, onRemoveClick, onCategoryOpen, remove, onRemove, category, snackbar, onCategoryAdd };
   }
 }
 </script>
